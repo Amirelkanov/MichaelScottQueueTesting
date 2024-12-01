a00fab0d0a1107ffb4b95826bb72f6eff3669d92
AmEl <amirelkanov0504@gmail.com>
2024-12-01 21:21:19 +0300

    Added stress tests for different num of threads

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -3,8 +3,8 @@ package org.example
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.check
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.junit.jupiter.api.Test
-
 
 class MSQueueTest {
     private val q = MSQueue()
@@ -16,9 +16,38 @@ class MSQueueTest {
     fun dequeue(): Int = q.dequeue()
 
     @Test
-    fun obstructionFreedomTest() {
+    fun obstructionFreedomTest() =
         ModelCheckingOptions()
             .checkObstructionFreedom(true)
             .check(this::class)
-    }
+
+    @Test
+    fun singleThreadStressTest() =
+        StressOptions()
+            .actorsBefore(10)
+            .threads(1)
+            .actorsPerThread(50)
+            .actorsAfter(10)
+            .iterations(100)
+            .check(this::class)
+
+    @Test
+    fun parallelThreadsStressTest() =
+        StressOptions()
+            .actorsBefore(5)
+            .threads(2)
+            .actorsPerThread(4)
+            .iterations(50)
+            .actorsAfter(2)
+            .check(this::class)
+
+    @Test
+    fun multiThreadStressTest() = // Больше не могу - хип заканчивается...)
+        StressOptions()
+            .actorsBefore(2)
+            .threads(10)
+            .actorsPerThread(1)
+            .actorsAfter(2)
+            .iterations(5)
+            .check(this::class)
 }
